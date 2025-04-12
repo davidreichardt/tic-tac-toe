949ad64bcc8a9776da13377ffaf0a286c20de31c
David Reichardt <davereichardt@gmail.com>
2025-04-12 14:57:25 -0400

    checkWin function, and refactored console.logs for clarity

--- tictactoe.js
+++ tictactoe.js
@@ -20,14 +20,14 @@ function GameBoard() {
   const placeMarker = (index, player) => {
     //if the board at the specified index is empty, place the current player's marker
     if (board[index] === '') {
-      console.log(index);
+      console.log(`placeMarker index = ${index}`);
       board[index] = player.marker;
       //return true to indicate success, else return false
       return true;
     } else {
       return false;
     }
-  }
+  };
 
   //a variable that holds each winning combo as an array
   const winningCombos = [
@@ -41,21 +41,39 @@ function GameBoard() {
     [2, 4, 6], //diagonals
   ];
 
+  const checkWin = (player) => {
+    //loop through winningCombos
+    for (combo of winningCombos) {
+      //deconstruct combo to get a, b, and c as indexes
+      //declare a variable for the player.marker
+      [a, b, c] = combo;
+      const mark = player.marker;
+      //check for a winning combo
+      if (board[a] === mark && board[b] === mark && board[c] === mark) {
+        //return true if a winning combo is found, breaking the loop
+        console.log(`The Winning Combo is: ${combo}`);
+        return true;
+      }
+    }
+    //if no win is found, return false
+    console.log('No win yet');
+    return false;
+  };
+
   //A function to see if no moves are available, and no win state (a draw)
   const checkDraw = () => {
     board.forEach((index) => {
       if (index !== null && index !== '') {
         draw = true;
-        console.log('draw', draw);
+        console.log(`draw = ${draw}`);
         return draw;
       } else {
-        console.log('draw', draw);
+        console.log(`draw = ${draw}`);
       }
     });
   };
 
-  //return everything to be used
-  return { createBoard, board, placeMarker, checkDraw };
+  return { createBoard, board, placeMarker, checkWin, checkDraw }; //return everything to be used
 }
 
 //A function to make a player object
@@ -68,12 +86,12 @@ function Player(name, marker) {
 const game = GameBoard();
 game.createBoard();
 // console.log(game.checkDraw());
-// console.log(checkWin());
 // console.log(game.placeMarker());
 const player1 = Player('Player 1', 'X');
 const player2 = Player('Player 2', 'O');
 // console.log(player1);
 // console.log(player2);
-
-console.log(game.placeMarker(3, player1), game.board);
-console.log(game.placeMarker(3, player2));
+console.log(game.placeMarker(2, player1), game.board);
+console.log(game.placeMarker(4, player1), game.board);
+console.log(game.placeMarker(7, player1), game.board);
+console.log(game.checkWin(player1));
